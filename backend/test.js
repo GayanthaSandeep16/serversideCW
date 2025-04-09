6061dd153947fb12d2cf7097a76c79a49eddc929
GayanthaSandeep16 <gayantha.20210118@iit.ac.lk>
2025-04-10 05:28:21 +0530

    Remove frontend application files and add backend Docker configuration

--- backend/test.js
+++ backend/test.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./database.sqlite', (err) => {
+const db = new sqlite3.Database('../database.sqlite', (err) => {
   if (err) {
     console.error("Error opening database:", err.message);
   } else {
